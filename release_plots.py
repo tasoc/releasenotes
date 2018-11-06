ca5f44cd22c6775cf610038cf1c6f9f861012128
Mikkel Nørup Lund <mikkelnl@phys.au.dk>
2018-11-06 22:47:28 +0100

    Release 2 plots

--- release_plots.py
+++ release_plots.py
@@ -23,6 +23,7 @@ rc('text', usetex=True)
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 import scipy.interpolate as INT
+from bottleneck import move_std
 from astropy.io import fits
 from statsmodels.nonparametric.kde import KDEUnivariate as KDE
 #from pywcsgrid2.allsky_axes import make_allsky_axes_from_header, allsky_header
@@ -157,11 +158,218 @@ def compute_onehour_rms(flux, cad):
 #
 # =============================================================================
 
+def plot_bg(data_paths, sector, cad=1800, sysnoise=0, version=1, savetex=False):
+	norm = colors.Normalize(vmin=1, vmax=4)
+	scalarMap = cmx.ScalarMappable(norm=norm, cmap=plt.get_cmap('Set1') )
 
-def plot_onehour_noise(data_paths, sector, cad=1800, sysnoise=0):
+
+	fig = plt.figure(figsize=(15, 6))
+	ax1 = fig.add_subplot(221)
+	ax2 = fig.add_subplot(222)
+	ax3 = fig.add_subplot(223)
+	ax4 = fig.add_subplot(224)
+	ax1.set_rasterization_zorder(0)
+	ax2.set_rasterization_zorder(0)
+	ax3.set_rasterization_zorder(0)
+	ax4.set_rasterization_zorder(0)
+	
+	fig.subplots_adjust(left=0.08, wspace=0.3, top=0.945, bottom=0.145, right=0.98)
+	
+	for k, d in enumerate(data_paths):
+		
+		files = np.array([])
+		for root, dirs, fil in os.walk(d):
+			for file in fil:
+				file_path = root + os.sep + file
+				if ('corr' in file_path) and ('.fits' in file_path):
+					files = np.append(files, file_path)
+					
+		for i, f in enumerate(files):
+			with fits.open(f) as hdu:
+				time = hdu[1].data['TIME']
+				bg = hdu[1].data['FLUX_BKG']
+
+				cam = hdu[0].header['CAMERA']
+				
+				print(cam)
+				rgba_color = scalarMap.to_rgba(cam)
+				if cam==1:
+					ax1.scatter(time, bg, marker='o', s=1, facecolors='None', edgecolor=rgba_color, zorder=-1)
+				if cam==2:
+					ax2.scatter(time, bg, marker='o', s=1, facecolors='None', edgecolor=rgba_color, zorder=-1)
+				if cam==3:
+					ax3.scatter(time, bg, marker='o', s=1, facecolors='None', edgecolor=rgba_color, zorder=-1)
+				if cam==4:
+					ax4.scatter(time, bg, marker='o', s=1, facecolors='None', edgecolor=rgba_color, zorder=-1)	
+#				ax.scatter(rms_tmag_vals[idx_lc, 0], rms_tmag_vals[idx_lc, 2], marker='s', facecolors='None', edgecolor=rgba_color)
+
+#	ax4.set_xlim([np.min(times)-500, np.max(times)+500])
+#	ax4.set_ylim([-0.15, 0.15])
+					
+	ax1.text(0.05, 0.9, 'Camera 1', transform=ax1.transAxes, fontsize=14)				
+	ax2.text(0.05, 0.9, 'Camera 2', transform=ax2.transAxes, fontsize=14)				
+	ax3.text(0.05, 0.9, 'Camera 3', transform=ax3.transAxes, fontsize=14)				
+	ax4.text(0.05, 0.9, 'Camera 4', transform=ax4.transAxes, fontsize=14)
+				
+	ax3.set_xlabel('Time TBJD (days)', fontsize=16, labelpad=10)
+	ax4.set_xlabel('Time TBJD (days)', fontsize=16, labelpad=10)
+	ax1.set_ylabel(r'$\rm Counts\,\, (e^{-}/s)$', fontsize=16, labelpad=10)
+	ax3.set_ylabel(r'$\rm Counts\,\, (e^{-}/s)$', fontsize=16, labelpad=10)
+	
+#	ax2.set_ylim([2200, 8000])
+	for ax in np.array([ax1, ax2, ax3, ax4]):
+		ax.xaxis.set_major_locator(MultipleLocator(5))
+		ax.xaxis.set_minor_locator(MultipleLocator(2.5))
+		ax.tick_params(direction='out', which='both', pad=5, length=3)
+		ax.tick_params(which='major', pad=6, length=5,labelsize='15')
+
+	
+	if version!=1:
+		save_path = 'plots/sector%02d/v%1d/' %(sector,version)
+	else:
+		save_path = 'plots/sector%02d/' %sector
+
+	if not os.path.exists(save_path):
+		os.makedirs(save_path)
+	fig.savefig(os.path.join(save_path, 'BG.pdf'), bb_inches='tight')		
+	fig.savefig(os.path.join(save_path, 'BG.png'), bb_inches='tight')		
+		
+	if savetex:
+		save_path2 = '../releasenote_tex/Release_note%1d/' %sector
+		fig.savefig(os.path.join(save_path2, 'BG.pdf'), bb_inches='tight')
+
+
+	plt.show()
+# =============================================================================
+# 
+# =============================================================================
+
+def plot_noice_lc(data_paths, sector, cad=1800, sysnoise=0, version=1, savetex=False):
 
 	norm = colors.Normalize(vmin=0, vmax=len(data_paths)-1)
-	scalarMap = cmx.ScalarMappable(norm=norm, cmap=plt.get_cmap('tab10') )
+
+	fig2 = plt.figure()
+	fig2.subplots_adjust(left=0.18, wspace=0.3, top=0.97, bottom=0.145, right=0.97)
+	ax2 = fig2.add_subplot(111)
+	
+	fig3 = plt.figure()
+	fig3.subplots_adjust(left=0.18, wspace=0.3, top=0.97, bottom=0.145, right=0.97)
+	ax3 = fig3.add_subplot(111)
+	
+	fig4 = plt.figure()
+	fig4.subplots_adjust(left=0.18, wspace=0.3, top=0.97, bottom=0.145, right=0.97)
+	ax4 = fig4.add_subplot(111)
+
+
+	times = np.array([]); c1s = np.array([]); c2s = np.array([])
+	for k, d in enumerate(data_paths):
+		
+		files = np.array([])
+		for root, dirs, fil in os.walk(d):
+			for file in fil:
+				file_path = root + os.sep + file
+				if ('corr' in file_path) and ('.fits' in file_path):
+					print(file_path)
+					files = np.append(files, file_path)
+					
+
+		for i, f in enumerate(files):
+			with fits.open(f) as hdu:
+				
+				c1 = hdu[1].data['MOM_CENTR1']
+				c2 = hdu[1].data['MOM_CENTR2']
+				time = hdu[1].data['CADENCENO']
+				
+				if '00279741379' in f:
+					ax4.plot(time-time[0], c2-np.nanmedian(c2), ls='-',color='k', label='row')
+					ax4.plot(time-time[0], c1-np.nanmedian(c1), ls='-',color='r', label='column')
+
+				times = np.append(times, time)
+				c1s = np.append(c1s, c1-np.nanmedian(c1))
+				c2s = np.append(c2s, c2-np.nanmedian(c2))
+			
+		
+	idx = np.argsort(times)	
+	c1s = c1s[idx]
+	c2s = c2s[idx]
+	times = times[idx]
+	times -= times[0]
+	
+	ax2.plot(times, c2s, ls='-',color='k', label='row')
+	ax2.plot(times, c1s, ls='-',color='r', label='column')
+	
+#	ax2.scatter(times, c2s, marker='o',color='k', alpha=0.1, label='row')
+#	ax2.scatter(times, c1s, marker='o',color='r', alpha=0.1, label='column')
+
+	idx_zoom = (times<15870) & (times>15840)
+	ax3.scatter((times[idx_zoom]-15855), c2s[idx_zoom], marker='o', edgecolors='k', facecolors='none',label='row')
+	ax3.scatter((times[idx_zoom]-15855), c1s[idx_zoom], marker='o' ,edgecolors='r', facecolors='none',label='column')
+	ax3.set_xlabel('Cadence number - 15855', fontsize=16, labelpad=10)
+	ax3.set_ylabel(r'$\rm Relative\,\, position\,\, (pixels)$', fontsize=16, labelpad=10)
+	ax3.xaxis.set_major_locator(MultipleLocator(5))
+	ax3.xaxis.set_minor_locator(MultipleLocator(1))
+	ax3.tick_params(direction='out', which='both', pad=5, length=3)
+	ax3.tick_params(which='major', pad=6, length=5,labelsize='15')
+	ax3.yaxis.set_ticks_position('both')
+	ax3.legend(loc='upper right')
+	
+	
+	ax4.set_xlim([np.min(times)-500, np.max(times)+500])
+	ax4.set_ylim([-0.15, 0.15])
+	ax4.set_xlabel('Cadence number', fontsize=16, labelpad=10)
+	ax4.set_ylabel(r'$\rm Relative\,\, position\,\, (pixels)$', fontsize=16, labelpad=10)
+	ax4.xaxis.set_major_locator(MultipleLocator(5000))
+	ax4.xaxis.set_minor_locator(MultipleLocator(2500))
+	ax4.tick_params(direction='out', which='both', pad=5, length=3)
+	ax4.tick_params(which='major', pad=6, length=5,labelsize='15')
+	ax4.yaxis.set_ticks_position('both')
+	ax4.legend(loc='upper right')
+
+#	###########
+	ax2.set_xlim([np.min(times)-500, np.max(times)+500])
+	ax2.set_ylim([-0.3,0.3])
+	ax2.set_xlabel('Cadence number', fontsize=16, labelpad=10)
+	ax2.set_ylabel(r'$\rm Relative\,\, position\,\, (pixels)$', fontsize=16, labelpad=10)
+	ax2.xaxis.set_major_locator(MultipleLocator(5000))
+	ax2.xaxis.set_minor_locator(MultipleLocator(2500))
+	ax2.tick_params(direction='out', which='both', pad=5, length=3)
+	ax2.tick_params(which='major', pad=6, length=5,labelsize='15')
+	ax2.yaxis.set_ticks_position('both')
+	ax2.legend(loc='upper right')
+
+	if version!=1:
+		save_path = 'plots/sector%02d/v%1d/' %(sector,version)
+	else:
+		save_path = 'plots/sector%02d/' %sector
+
+	if not os.path.exists(save_path):
+		os.makedirs(save_path)
+		
+	fig2.savefig(os.path.join(save_path, 'pixelpos.pdf'), bb_inches='tight')
+	fig3.savefig(os.path.join(save_path, 'pixelpos_zoom.pdf'), bb_inches='tight')
+	fig4.savefig(os.path.join(save_path, 'pixel_specific.pdf'), bb_inches='tight')
+	fig2.savefig(os.path.join(save_path, 'pixelpos.png'), bb_inches='tight')
+	fig3.savefig(os.path.join(save_path, 'pixelpos_zoom.png'), bb_inches='tight')
+	fig4.savefig(os.path.join(save_path, 'pixel_specific.png'), bb_inches='tight')
+	
+	
+	if savetex:
+		save_path2 = '../releasenote_tex/Release_note%1d/' %sector
+		fig2.savefig(os.path.join(save_path2, 'pixelpos.pdf'), bb_inches='tight')
+		fig3.savefig(os.path.join(save_path2, 'pixelpos_zoom.pdf'), bb_inches='tight')
+		fig4.savefig(os.path.join(save_path2, 'pixel_specific.pdf'), bb_inches='tight')
+	
+
+	plt.show()
+
+# =============================================================================
+# 
+# =============================================================================
+
+def plot_onehour_noise(data_paths, sector, cad=1800, sysnoise=0, version=1, savetex=False, labels=None):
+
+	norm = colors.Normalize(vmin=0, vmax=len(data_paths)+5)
+	scalarMap = cmx.ScalarMappable(norm=norm, cmap=plt.get_cmap('Set1') )
 
 
 	fig = plt.figure()
@@ -234,11 +442,15 @@ def plot_onehour_noise(data_paths, sector, cad=1800, sysnoise=0):
 
 		rgba_color = scalarMap.to_rgba(k)
 
+		if not labels is None:
+			lab = labels[k]
+		else:
+			lab=''
 
-		ax.scatter(rms_tmag_vals[idx_sc, 0], rms_tmag_vals[idx_sc, 1], marker='o', facecolors='None', edgecolor=rgba_color)
+		ax.scatter(rms_tmag_vals[idx_sc, 0], rms_tmag_vals[idx_sc, 1], marker='o', facecolors='None', edgecolor=rgba_color, label=lab)
 		ax.scatter(rms_tmag_vals[idx_lc, 0], rms_tmag_vals[idx_lc, 2], marker='s', facecolors='None', edgecolor=rgba_color)
 
-		ax2.scatter(rms_tmag_vals[idx_sc, 0], rms_tmag_vals[idx_sc, 3], marker='o', facecolors='None', edgecolor=rgba_color)
+		ax2.scatter(rms_tmag_vals[idx_sc, 0], rms_tmag_vals[idx_sc, 3], marker='o', facecolors='None', edgecolor=rgba_color, label=lab)
 		ax2.scatter(rms_tmag_vals[idx_lc, 0], rms_tmag_vals[idx_lc, 4], marker='s', facecolors='None', edgecolor=rgba_color)
 
 	# Plot theoretical lines
@@ -316,10 +528,16 @@ def plot_onehour_noise(data_paths, sector, cad=1800, sysnoise=0):
 	ax2.tick_params(which='major', pad=6, length=5,labelsize='15')
 	ax2.yaxis.set_ticks_position('both')
 
-#	plt.tight_layout()
 
-	save_path = 'plots/sector%02d/' %sector
-	save_path2 = '../releasenote_tex/Release_note%1d/' %sector
+	ax.legend(loc='upper left', prop={'size': 12})
+	ax2.legend(loc='upper left', prop={'size': 12})
+#	plt.tight_layout()
+	
+	if version!=1:
+		save_path = 'plots/sector%02d/v%1d/' %(sector,version)
+	else:
+		save_path = 'plots/sector%02d/' %sector
+
 	if not os.path.exists(save_path):
 		os.makedirs(save_path)
 		
@@ -328,10 +546,11 @@ def plot_onehour_noise(data_paths, sector, cad=1800, sysnoise=0):
 	fig2.savefig(os.path.join(save_path, 'mvd_noise.pdf'), bb_inches='tight')
 	fig2.savefig(os.path.join(save_path, 'mvd_noise.png'), bb_inches='tight')
 	
-	fig.savefig(os.path.join(save_path2, 'rms_noise.pdf'), bb_inches='tight')
-	fig.savefig(os.path.join(save_path2, 'rms_noise.png'), bb_inches='tight')
-	fig2.savefig(os.path.join(save_path2, 'mvd_noise.pdf'), bb_inches='tight')
-	fig2.savefig(os.path.join(save_path2, 'mvd_noise.png'), bb_inches='tight')
+	if savetex:
+		save_path2 = '../releasenote_tex/Release_note%1d/' %sector
+
+		fig.savefig(os.path.join(save_path2, 'rms_noise.pdf'), bb_inches='tight')
+		fig2.savefig(os.path.join(save_path2, 'mvd_noise.pdf'), bb_inches='tight')
 
 	plt.show()
 
@@ -339,37 +558,49 @@ def plot_onehour_noise(data_paths, sector, cad=1800, sysnoise=0):
 #
 # =============================================================================
 
-def plot_pixinaperture(data_path, sector, cad=1800, sysnoise=0):
+def plot_pixinaperture(data_paths, sector, cad=1800, sysnoise=0, version=1, savetex=False, labels=None):
 
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
 
-	# Add data values
-#	files = np.array([os.path.join(data_path, f) for f in os.listdir(data_path) if f.endswith('.fits.gz')])
-	files = np.array([])
-	for root, dirs, fil in os.walk(data_path):
-		for file in fil:
-			file_path = root + os.sep + file
-			if ('corr' in file_path) and ('.fits' in file_path):
-				print(file_path)
-				files = np.append(files, file_path)
+	norm = colors.Normalize(vmin=0, vmax=len(data_paths)+5)
+	scalarMap = cmx.ScalarMappable(norm=norm, cmap=plt.get_cmap('Set1') )
 
+	
+	for k, d in enumerate(data_paths):
+	
+		files = np.array([])
+		for root, dirs, fil in os.walk(d):
+			for file in fil:
+				file_path = root + os.sep + file
+				if ('corr' in file_path) and ('.fits' in file_path):
+					print(file_path)
+					files = np.append(files, file_path)
+	
+	
+		ap_tmag_vals = np.zeros([len(files), 2])
+		for i, f in enumerate(files):
+			hdu = fits.open(f)
+			tmag = hdu[0].header['TESSMAG']
+			ap = hdu[3].data
+	
+			in_aperture = (ap & 2+8 != 0)
+	#		print(in_aperture)
+			ap_tmag_vals[i, 0] = tmag
+			ap_tmag_vals[i, 1] = np.sum(in_aperture)
+			if np.sum(in_aperture)==15:
+				print(hdu[0].header['TICID'])
+	
+		# Colour by distance from camera centre
+		
+		rgba_color = scalarMap.to_rgba(k)
 
-	ap_tmag_vals = np.zeros([len(files), 2])
-	for i, f in enumerate(files):
-		hdu = fits.open(f)
-		tmag = hdu[0].header['TESSMAG']
-		ap = hdu[3].data
-
-		in_aperture = (ap & 2+8 != 0)
-#		print(in_aperture)
-		ap_tmag_vals[i, 0] = tmag
-		ap_tmag_vals[i, 1] = np.sum(in_aperture)
-		if np.sum(in_aperture)==15:
-			print(hdu[0].header['TICID'])
-
-	# Colour by distance from camera centre
-	ax.scatter(ap_tmag_vals[:, 0], ap_tmag_vals[:, 1], marker='o', facecolors='None', color='k')
+		if not labels is None:
+			lab = labels[k]
+		else:
+			lab=''
+			
+		ax.scatter(ap_tmag_vals[:, 0], ap_tmag_vals[:, 1], marker='o', facecolors='None', color=rgba_color, label=lab)
 
 	mags = np.linspace(3.5, 16.5, 500)
 	pix = np.asarray([Pixinaperture(m) for m in mags], dtype='float64')
@@ -390,15 +621,24 @@ def plot_pixinaperture(data_path, sector, cad=1800, sysnoise=0):
 	ax.yaxis.set_ticks_position('both')
 	plt.tight_layout()
 
-	save_path = 'plots/sector%02d/' %sector
-	save_path2 = '../releasenote_tex/Release_note%1d/' %sector
+	ax.legend(loc='upper left', prop={'size': 12})
+
+	if version!=1:
+		save_path = 'plots/sector%02d/v%1d/' %(sector,version)
+	else:
+		save_path = 'plots/sector%02d/' %sector
+
 	if not os.path.exists(save_path):
 		os.makedirs(save_path)
+
+
 	fig.savefig(os.path.join(save_path, 'pix_in_aperture.pdf'), bb_inches='tight')
 	fig.savefig(os.path.join(save_path, 'pix_in_aperture.png'), bb_inches='tight')
 	
-	fig.savefig(os.path.join(save_path2, 'pix_in_aperture.pdf'), bb_inches='tight')
-	fig.savefig(os.path.join(save_path2, 'pix_in_aperture.png'), bb_inches='tight')
+	if savetex:
+		save_path2 = '../releasenote_tex/Release_note%1d/' %sector
+
+		fig.savefig(os.path.join(save_path2, 'pix_in_aperture.pdf'), bb_inches='tight')
 
 	plt.show()
 
@@ -407,7 +647,7 @@ def plot_pixinaperture(data_path, sector, cad=1800, sysnoise=0):
 #
 # =============================================================================
 
-def plot_mag_dist(data_path, sector):
+def plot_mag_dist(data_path, sector, version=1, savetex=False):
 
 	# Add data values
 	files = np.array([])
@@ -476,20 +716,21 @@ def plot_mag_dist(data_path, sector):
 
 	ax.yaxis.set_ticks_position('both')
 
+	if version!=1:
+		save_path = 'plots/sector%02d/v%1d/' %(sector,version)
+	else:
+		save_path = 'plots/sector%02d/' %sector
 
-	save_path = 'plots/sector%02d/' %sector
-	save_path2 = '../releasenote_tex/Release_note%1d/' %sector
-	
 	if not os.path.exists(save_path):
 		os.makedirs(save_path)
 
-	fig.savefig(os.path.join(save_path, 'magnitudes.png') )
 	fig.savefig(os.path.join(save_path, 'magnitudes.pdf'), bb_inches='tight')
 	fig.savefig(os.path.join(save_path, 'magnitudes.png'), bb_inches='tight')
 	
-	fig.savefig(os.path.join(save_path2, 'magnitudes.png') )
-	fig.savefig(os.path.join(save_path2, 'magnitudes.pdf'), bb_inches='tight')
-	fig.savefig(os.path.join(save_path2, 'magnitudes.png'), bb_inches='tight')
+	if savetex:
+		save_path2 = '../releasenote_tex/Release_note%1d/' %sector
+			
+		fig.savefig(os.path.join(save_path2, 'magnitudes.pdf'), bb_inches='tight')
 	
 	
 	plt.show()
@@ -604,14 +845,18 @@ if __name__ == "__main__":
 
 	plt.close('all')
 
-	path0 = '/home/mikkelnl/ownCloud/Documents/Asteroseis/TESS/TASOC_code/Release_data/Sector2/output/tpf/'
+	
+
+	path1 = '/home/mikkelnl/ownCloud/Documents/Asteroseis/TESS/TASOC_code/Release_data/Sector2/output/tpf/'
+	path0 = '/home/mikkelnl/ownCloud/Documents/Asteroseis/TESS/TASOC_code/Release_data/Sector1/output/tpf/'
 #	path0 = r'C:\Users\au195407\Downloads\Ny mappe'
 #	data_paths = np.array([path0 + '08', path0 + '09', path0 + '10', path0 + '11', path0 + '12'])
 	#data_paths = np.array([path0 + '10',]
 
-	plot_onehour_noise([path0], cad=1800, sector=2, sysnoise=0)
-	plot_pixinaperture(path0, sector=2)
-	plot_mag_dist(path0, sector=2)
-#	data_path = '/home/mikkelnl/ownCloud/Documents/Asteroseis/Emil/TESS_alerts/'
+	plot_bg([path1], cad=1800, sector=2, version=1, sysnoise=0, savetex=False)
+	plot_noice_lc([path1], cad=1800, sector=2, version=1, sysnoise=0, savetex=False)
+	plot_onehour_noise([path0, path1], cad=1800, sector=2, version=1, sysnoise=0, savetex=True, labels=np.array(['Sector 1', 'Sector 2']))
+	plot_pixinaperture([path0, path1], sector=2, version=1, savetex=True, labels=np.array(['Sector 1', 'Sector 2']))
+	plot_mag_dist(path1, sector=2, version=1, savetex=False)
 
 	
